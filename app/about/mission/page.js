5aa146fbe28dcf08c1335431e1d7876f23e4affd
Mahdi Mortuza <mahdi.webx@gmail.com>
2024-01-03 10:47:13 +0600

    image optimiization

--- app/about/mission/page.js
+++ app/about/mission/page.js
@@ -1,17 +1,18 @@
 
 
 import Button from "@/app/components/Button";
+import car from "@/public/image/car.jpg";
+import Image from "next/image";
 
 const mission = () => {
     return (
 <main>
 <div>this is mission page.</div>
 
-<div className="mt-5">
-
-
-</div>
+<div className="mt-5 w-[600px]">
+<Image placeholder="blur" src={car} alt="car "/>
 <Button />
+</div>
 </main>
 );
 };
