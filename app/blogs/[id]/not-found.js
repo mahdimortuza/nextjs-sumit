86937c976a27745c496d88aa3c52c5fcc001bb58
Mahdi Mortuza <mahdi.webx@gmail.com>
2024-01-03 10:08:07 +0600

    not found

---
+++ app/blogs/[id]/not-found.js
@@ -0,0 +1,10 @@
+
+const NotFound = () => {
+    return (
+        <div className="mt-6">
+            blog is not found
+        </div>
+    );
+};
+
+export default NotFound;
--- app/blogs/[id]/page.js
+++ app/blogs/[id]/page.js
@@ -1,6 +1,10 @@
+import { notFound } from "next/navigation";
 
 const BlogPage = ({params}) => {
     const {id} = params
+    if(id === "3"){
+        notFound()
+    }
     return (
         <div className="mt-6">
             the blog id is {id}
